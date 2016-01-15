0e751ecb7cdb808359acaf1058bc4c4f8c293f7d
uponuponupon <mikechen.csb@gmail.com>
2016-01-15 14:38:05 +0800

    function test

--- test/function.js
+++ test/function.js
@@ -10,15 +10,15 @@ function foo2(a, b) {
 console.log(foo2(1,2));
 
 // function expression
-var a = function foo3(a, b) {
-    var c = a - b;
-    return c;
+var d = function foo3(e, f) {
+    var g = e - f;
+    return g;
 }
-console.log(a(1,2));
+console.log(d(1,2));
 
 // anonymous
-var b = function(a, b) {
-    var c = a * b;
-    return c;
+var h = function(i, j) {
+    var k = i * j;
+    return k;
 }
-console.log(b(1,2));
+console.log(h(1,2));
